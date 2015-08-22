90dc1b4c90e74309de7c9f68defe74f39ff05274
Alistair Shaw <alistairshaw@gmail.com>
2015-08-22 18:39:20 +0100

    Wrapper classes

--- resources/views/cms/page.blade.php
+++ resources/views/cms/page.blade.php
@@ -1,20 +1,22 @@
 @extends('vendirun::layouts.standard')
 @section('content')
     @foreach ($page->page_blocks as $block)
-        <div class="container{{ $block->full_width ? '-fluid' : '' }} {{ $block->class }}">
-            <div class="row">
-                <div style="margin: {{ $block->margin }}; padding: {{ $block->padding }};">
-                    @foreach ($block->elements as $element)
-                        <div class="{{ $block->full_width ? '' : 'col-sm-' . $element->column_width }} {{ $element->class }}">
-                            @if ($element->type == 'widget')
-                                @include('vendirun::cms.widgets.' . $element->content)
-                            @elseif ($element->type == 'html')
-                                {!! $element->content !!}
-                            @elseif ($element->type == 'image')
-                                <img src="{{ $element->content }}" <?php if ($element->height > 0) echo 'style="height: ' . $element->height . 'px; width: ' . $element->width . 'px;"' ?> class="img-responsive">
-                            @endif
-                        </div>
-                    @endforeach
+        <div class="{{ $block->class }}">
+            <div class="container{{ $block->full_width ? '-fluid' : '' }}">
+                <div class="row">
+                    <div style="margin: {{ $block->margin }}; padding: {{ $block->padding }};">
+                        @foreach ($block->elements as $element)
+                            <div class="{{ $block->full_width ? '' : 'col-sm-' . $element->column_width }} {{ $element->class }}">
+                                @if ($element->type == 'widget')
+                                    @include('vendirun::cms.widgets.' . $element->content)
+                                @elseif ($element->type == 'html')
+                                    {!! $element->content !!}
+                                @elseif ($element->type == 'image')
+                                    <img src="{{ $element->content }}" <?php if ($element->height > 0) echo 'style="height: ' . $element->height . 'px; width: ' . $element->width . 'px;"' ?> class="img-responsive">
+                                @endif
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>

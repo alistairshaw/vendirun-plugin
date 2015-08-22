9a922c50f6fbb55d303e67caed3ddb6db65870c8
Alistair Shaw <alistairshaw@gmail.com>
2015-08-22 19:53:35 +0100

    Added class to all blocks

--- resources/views/cms/page.blade.php
+++ resources/views/cms/page.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     @foreach ($page->page_blocks as $block)
         <div class="{{ $block->class }}">
-            <div class="container{{ $block->full_width ? '-fluid' : '' }}">
+            <div class="container{{ $block->full_width ? '-fluid' : '' }} element-{{ $element->type }}">
                 <div class="row">
                     <div class="clearfix" style="margin: {{ $block->margin }}; padding: {{ $block->padding }};">
                         @foreach ($block->elements as $element)

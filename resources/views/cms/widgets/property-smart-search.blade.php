810ebfa4b1b6ef518c29a30d2a4bbfb205fe7982
Alistair Shaw <alistairshaw@gmail.com>
2015-08-22 18:01:59 +0100

    Search box options

--- resources/views/cms/widgets/property-smart-search.blade.php
+++ resources/views/cms/widgets/property-smart-search.blade.php
@@ -1,10 +1,11 @@
+<?php $options = json_decode($element->element_options, true) ?>
 <form method="post" action="{{ route('vendirun.propertySearch') }}" class="search-box">
     <div class="form-group">
         <label class="sr-only">Property Search</label>
-        <input type="text" class="form-control" name="search_string" placeholder="e.g. 2 Bedroom Apartment in Marbella">
+        <input type="text" class="form-control" name="search_string" placeholder="{{ $options['placeholder'] or '' }}">
     </div>
     <div class="form-group">
         <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-        <button type="submit" class="btn btn-default">Start your search now</button>
+        <button type="submit" class="btn btn-default">{{ $options['button_text'] or 'Search Now' }}</button>
     </div>
 </form>

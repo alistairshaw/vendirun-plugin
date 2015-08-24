14bd72743e2ffb77ba733965656d4152f687c7e6
Alistair Shaw <alistairshaw@gmail.com>
2015-08-24 19:59:03 +0100

    Typo

--- resources/views/property/search.blade.php
+++ resources/views/property/search.blade.php
@@ -62,7 +62,7 @@
 
             </div>
             <div class="popout hidden-sm hidden-xs">
-                @include('vendirun::property.partials.filter-button)
+                @include('vendirun::property.partials.filter-button')
             </div>
             <div class="col-sm-2 hide left-column hidden-sm hidden-xs">
                 <div class="well refine-search">

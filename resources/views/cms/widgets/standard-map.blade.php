8ab1902d71dbdbb90088fb08ce144d42fa5559a1
Alistair Shaw <alistairshaw@gmail.com>
2015-12-30 23:13:41 +0000

    CSRF Token

--- resources/views/cms/widgets/standard-map.blade.php
+++ resources/views/cms/widgets/standard-map.blade.php
@@ -1,3 +1,4 @@
 <?php if (!isset($options)) $options = json_decode($element->element_options, true) ?>
 <input type="hidden" id="googleMapAddress" value="{{ $options['address'] }}">
 <div id="map_canvas" class="map-canvas"></div>
+<input type="hidden" id="_token" value="{{ csrf_token() }}" />

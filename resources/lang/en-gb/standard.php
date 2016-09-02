ffb956352483a12fbcf14e58d8855f6b4dade424
Alistair Shaw <alistairshaw@gmail.com>
2016-09-02 21:55:26 +0100

    404 error pages fixed

--- resources/lang/en-gb/standard.php
+++ resources/lang/en-gb/standard.php
@@ -16,6 +16,8 @@ return [
     'currentlyLoggedIn' => 'You are currently logged in',
     'clickToLogOut' => 'Click here to log out',
     'fourOhFour' => 'I\'m afraid this page could not be found :(',
+    'fiveHundred' => 'Looks like we\'re having server problems at our end, please wait a few minutes and try again.',
+    'fiveOhThree' => 'Temporary service outage, we\'ll be up and running again shortly.',
     'share' => 'Share',
     'priceOnRequest' => 'Price on Request',
     'shareThis' => 'Share This'

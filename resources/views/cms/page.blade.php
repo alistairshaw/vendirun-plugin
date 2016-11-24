989394ecc99fc7f7f381781d88d1fc1c15d539c2
Alistair Shaw <alistairshaw@gmail.com>
2016-11-24 16:23:39 +0000

    VLP-40: Empty block handler

--- resources/views/cms/page.blade.php
+++ resources/views/cms/page.blade.php
@@ -11,7 +11,9 @@
                     <div class="clearfix" style="margin: {{ $block->margin }}; padding: {{ $block->padding }};">
                         @foreach ($block->elements as $element)
                             <div class="{{ $block->full_width ? '' : 'col-sm-' . $element->column_width }} {{ $element->class }}">
+                                <?php try { ?>
                                 @include('vendirun::cms.elements.' . $element->type)
+                                <?php } catch (Exception $e) { echo '<code style="text-align: center; padding: 50px; display: block;" class="alert alert-info"><i class="fa fa-question-circle"></i> Empty Block</code>'; } ?>
                             </div>
                         @endforeach
                     </div>

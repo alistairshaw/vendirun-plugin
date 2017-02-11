3dfca05037e25c88961a85ed0633b577e58843d5
Alistair Shaw <alistairshaw@gmail.com>
2017-02-11 20:01:25 +0000

    VLP-61: Error fix

--- resources/views/cms/widgets/slider-call-to-action.blade.php
+++ resources/views/cms/widgets/slider-call-to-action.blade.php
@@ -1,5 +1,5 @@
 <div class="carousel-call-to-action carousel-call-to-action-slide-{{ $slide['id'] }}">
     <p>
-        <button class="btn">{{ $slide['call_to_action'] }}</button>
+        <button class="btn">{{ $slide['callToAction'] }}</button>
     </p>
 </div>

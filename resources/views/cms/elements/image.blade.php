4623cf4e97d95e9aa8450ea3131d2f4c29b13bd5
Alistair Shaw <alistairshaw@gmail.com>
2017-03-15 21:09:48 +0000

    VLP-89: Fix cms image element sizing

--- resources/views/cms/elements/image.blade.php
+++ resources/views/cms/elements/image.blade.php
@@ -2,7 +2,7 @@
     @if ($element->url)
         <a href="{{ $element->url }}">
             @endif
-            <img src="{{ $element->content }}" alt="{{ $element->alt }}" {{ $element->height > 0 ? 'style="height: ' . $element->height . 'px; width: ' . $element->width . 'px;"' : '' }} class="img-responsive">
+            <img src="{{ $element->content }}" alt="{{ $element->alt }}" {!! $element->height > 0 ? 'style="height: ' . $element->height . 'px; width: ' . $element->width . 'px;"' : '' !!} class="img-responsive">
             @if ($element->caption)
                 <div class="caption">
                     {{ $element->caption }}
